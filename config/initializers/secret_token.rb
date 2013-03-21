422fe77e4b083e9af8cdbe2108cd86781a4f7f30
Matthew Norton <mnort9@gmail.com>
2013-03-20 23:40:26 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crowdappraise::Application.config.secret_token = '03ca7fa3c39104e74ec484331593a88cb688a3eeb68856c61fd0f9ca227bc319e2739c5c75dbc2b99f7918d477beed2d9593f78326924d988aba14f94c782548'
