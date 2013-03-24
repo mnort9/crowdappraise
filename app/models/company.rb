c19170f7671cb85993f4575e84dcb4367209ec04
Matthew Norton <mnort9@gmail.com>
2013-03-24 15:22:59 -0400

    Before attempt at fixing imagemagick

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,12 @@
 class Company < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :image
+
+  validates :name, presence: true
+  validates_attachment :image, presence: true,
+  														 content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+  														 size: { less_than: 5.megabytes }
 
   has_many :opportunities
+  has_attached_file :image, styles: { medium: "320x240>" }
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130324180115) do
+ActiveRecord::Schema.define(:version => 20130324190055) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "opportunities", :force => true do |t|
