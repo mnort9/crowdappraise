be1c5e388dddcf213f2c23d3e9a70d9d6885a30a
Matthew Norton <mnort9@gmail.com>
2013-03-24 15:50:26 -0400

    Add Paperclip gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,7 @@ Crowdappraise::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
+  # Required for Papercilp
+  Paperclip.options[:command_path] = "/usr/local/bin/" 
+
 end
