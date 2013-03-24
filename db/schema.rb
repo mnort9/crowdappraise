edf323512d1d8ee9439419f9e52842d1004ff854
Matthew Norton <mnort9@gmail.com>
2013-03-24 14:17:16 -0400

    Add username to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130322021029) do
+ActiveRecord::Schema.define(:version => 20130324180115) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(:version => 20130322021029) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
