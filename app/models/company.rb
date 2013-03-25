b53d091c804acce468edac4a001b461b53c438a0
Matthew Norton <mnort9@gmail.com>
2013-03-25 13:22:18 -0400

    Add s3.yml config file. Fixed s3 integration by adding s3 info to company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -10,5 +10,8 @@ class Company < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: true
   has_attached_file :image,
-  									 styles: { medium: "320x240>" }
+  									 styles: { medium: "320x240>" },
+  									 :storage => :s3,
+     									:s3_credentials => "#{Rails.root}/config/s3.yml",
+     									:path => "/:style/:id/:filename"
 end
